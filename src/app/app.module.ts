6e501937c954d7790e70c644123bf28744022923
alexvanhell <alejandrodnl.cv@hotmail.com>
2019-04-19 15:29:28 -0500

    Previsualización de contraseñas, modal de alerta de confirmación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,15 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    library.add(fas);
+  }
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/shared/services/alert.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  passwordPreviews = {
+    first: true,
+    confirm: true
+  }
 
-  constructor() { }
+  /* firstStatus: boolean = true;
+  firstStatus2: boolean = true; */
+  passText: string = 'Contraseña';
+
+  constructor(
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+  }
+
+  /* fn() {
+    this.firstStatus ? 'cumple' : 'no cumple';
+
+    if (this.firstStatus) {
+      'cumple'
+    } else {
+      'no cumple'
+    }
+  } */
+
+  getIcon(value: boolean): string {
+    if (value) {
+      return 'eye';
+    } else {
+      return 'eye-slash';
+    }
+  }
+
+  signup(event: Event): void {
+    event.preventDefault();
+
+    this.alertService.show({
+      title: 'Super alerta!',
+      body: '¿Estás seguro de hackear la NASA?'
+    })
   }
 
 }
